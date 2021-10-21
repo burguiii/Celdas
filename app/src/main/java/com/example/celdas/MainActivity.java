65cbd9ae7b54377ab4ed136b7a87b127ae4a6cc8
burguiii <roberto.burguillo.nogales@alumnojoyfe.iepgroup.es>
2021-10-21 19:33:16 +0200

    Celdas Funcionales(Marcador No)

--- app/src/main/java/com/example/celdas/MainActivity.java
+++ app/src/main/java/com/example/celdas/MainActivity.java
@@ -3,23 +3,18 @@ package com.example.celdas;
 import static android.graphics.Color.rgb;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-
     /*
     --Colores-- rgb
-    Naranja-> 225,135,27
+    Blanco->247,246,243
     Gris-> 97,95,91
      */
-
-
-
 
     Button btn1,btn2,btn3,btn4;
 
@@ -28,10 +23,15 @@ public class MainActivity extends AppCompatActivity {
     int estado_btn3 = 0;
     int estado_btn4 = 0;
 
+    int taps = 0;
+
+    TextView DisplayTap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
 
         btn1 = findViewById(R.id.btn1);
@@ -39,37 +39,46 @@ public class MainActivity extends AppCompatActivity {
         btn3 = findViewById(R.id.btn3);
         btn4 = findViewById(R.id.btn4);
 
+        DisplayTap = findViewById(R.id.DisplayTap);
+
     }
 
     public void click_btn1(View view){
         cambiar_btn1();
         cambiar_btn2();
         cambiar_btn3();
+        taps++;
+        DisplayTap.setText(""+taps);
     }
 
     public void click_btn2(View view){
         cambiar_btn2();
         cambiar_btn4();
         cambiar_btn1();
+        taps++;
+        DisplayTap.setText(""+ taps);
     }
 
     public void click_btn3(View view){
         cambiar_btn3();
         cambiar_btn4();
         cambiar_btn1();
-
+        taps++;
+        DisplayTap.setText(""+ taps);
     }
 
     public  void  click_btn4(View view){
         cambiar_btn4();
         cambiar_btn3();
         cambiar_btn2();
+        taps++;
+        DisplayTap.setText(""+ taps);
     }
 
     public void cambiar_btn1(){
         if (estado_btn1 == 0){
             estado_btn1 = 1;
-            btn1.setBackgroundColor(rgb(225,135,27));
+            btn1.setBackgroundColor(rgb(247,246,243));
         }
         else{
             estado_btn1 = 0;
@@ -77,12 +86,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-
     private void cambiar_btn2(){
         if(estado_btn2 == 0){
             estado_btn2 = 1;
-            btn2.setBackgroundColor(rgb(225,135,27));
+            btn2.setBackgroundColor(rgb(247,246,243));
         }
         else {
             estado_btn2 = 0;
@@ -93,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
     private void cambiar_btn3(){
         if(estado_btn3 == 0){
             estado_btn3 = 1;
-            btn3.setBackgroundColor(rgb(225,135,27));
+            btn3.setBackgroundColor(rgb(247,246,243));
         }
         else{
             estado_btn3 = 0;
@@ -101,12 +108,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-
     private void cambiar_btn4(){
         if(estado_btn4 == 0){
             estado_btn4 = 1;
-            btn4.setBackgroundColor(rgb(225,135,27));
+            btn4.setBackgroundColor(rgb(247,246,243));
         }
         else{
             estado_btn4 = 0;
